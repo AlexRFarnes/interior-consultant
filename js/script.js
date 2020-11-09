503479c58f0b949ca82a37dd92da84acfc39de9d
sudo-alex1990 <alexrfarnes@gmail.com>
2020-11-09 22:42:57 +0800

    remove padding on main-content and add transition to mobile nav items

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const toggleButton = document.getElementById("toggle-button");
 const nav = document.getElementById("mobile-nav");
+const navLinks = document.querySelectorAll('.mobile-navigation-item')
 
 toggleButton.addEventListener("click", () => {
+    // Toggle navigation 
     nav.classList.toggle("mobile-nav-active");
+
+    // Burger animation
     toggleButton.classList.toggle('toggle');
+
+    //Animate links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation="";
+        }else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 9 + 0.2}s`;
+        }
+    })
 })
